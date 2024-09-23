ee0f4b113b0ab8e77b1b1d142e03c24d7d5ca940
geekip <mail@yangfei.site>
2024-09-23 22:28:19 +0800

    update gfw

--- gfw/index.php
+++ gfw/index.php
@@ -7,13 +7,13 @@ define('FEED_URL', $_ENV['GFW_FEED_URL']);
 define('KEY', $_ENV['GFW_KEY']);
 
 // 基础配置文件
-define('BASE_CONFIG_FILE',  __DIR__ . 'base.yaml');
+define('BASE_CONFIG_FILE',  __DIR__ . '/base.yaml');
 // 基础配置缓存文件
-define('CONFIG_FILE',  __DIR__ . 'config');
+define('CONFIG_FILE',  __DIR__ . '/config');
 // 订阅缓存文件
-define('FEED_SOURCE',  __DIR__ . 'feed.source.yaml');
+define('FEED_SOURCE',  __DIR__ . '/feed.source.yaml');
 // 中国规则
-define('RULES_CN_CACHE',  __DIR__ . 'cn.json');
+define('RULES_CN_CACHE',  __DIR__ . '/cn.json');
 
 // 节点国家
 define('COUNTRIES', [

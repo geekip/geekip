23c2c5d84c461ad0f5d2c64234b71c525567109f
geekip <mail@yangfei.site>
2024-09-25 23:11:29 +0800

    update gfw

--- gfw/start.php
+++ gfw/start.php
@@ -2,18 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-// define('FEED_URL', $_ENV['GFW_FEED_URL']);
-
-// define('KEY', $_ENV['GFW_KEY']);
-
 // 基础配置文件
 define('BASE_CONFIG_FILE',  __DIR__ . '/base.yaml');
-// // 基础配置缓存文件
-// define('CONFIG_FILE',  __DIR__ . '/config');
-// // 订阅缓存文件
-// define('FEED_SOURCE',  __DIR__ . '/feed.source.yaml');
-// // 中国规则
-// define('RULES_CN_CACHE',  __DIR__ . '/cn.json');
 
 // 节点国家
 define('COUNTRIES', [
@@ -34,7 +24,6 @@ define('DICT', [
   'other'    => '其他节点',
   'private'  => '自建服务'
 ]);
-
 
 // 转换容量单位
 function ConvertSize($size) {
--- gfw/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-
-echo RULES_CN_CACHE;

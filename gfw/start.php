32f7e410a49516f39e63c68109f122c27cad4c37
geekip <mail@yangfei.site>
2024-11-01 22:23:36 +0800

    update

--- gfw/start.php
+++ gfw/start.php
@@ -364,7 +364,8 @@ function Get_base_config() {
 
 // 加密
 function encrypt($data, $key) {
-  return openssl_encrypt($data, EN_TYPE, $key, 0, 12345678);
+  $iv = hex2bin('34857d973953e44afb49ea9d61104d8c');
+  return openssl_encrypt($data, EN_TYPE, $key, 0, $iv);
 }
 
 $base_config = Get_base_config();
